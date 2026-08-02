f45d9fcd1442aca3496b12f08643d24f36c97b0d
by-albert <apencas03@gmail.com>
2026-08-02 12:47:51 +0200

    Cambios, ahora haré la nueva galeria

--- src/components/PortfolioCTA/PortfolioCTA.jsx
+++ src/components/PortfolioCTA/PortfolioCTA.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 const PortfolioCTA = ({ variant = "album" }) => {
 
+  // PORTAFOLIO CAJA
   return (
     <section className={`portfolio-hero ${variant}`}>
 
